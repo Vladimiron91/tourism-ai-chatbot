0695353ba027721b998b3c9740455b16cb4d9442
@vladimir_shevliaev <vladimirschevlaev@googlemail.com>
2026-01-22 00:54:48 +0100

    OpenAI Key mit Python verbunden

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#Variablen aus der .env Datei
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
